9afe0fbae76c8ad3536310265135fc66f1125324
Yury Usishchev <yury.usischev@phystech.edu>
2017-03-24 18:51:52 +0300

    remove unneeded

--- watchdog-5.15/include/xmalloc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _XMALLOC_H
-#define _XMALLOC_H
-
-void *xmalloc (size_t size);
-void *xcalloc (size_t nmemb, size_t size);
-char *xstrdup (const char *s);
-char *xstrndup (const char *s, int n);
-
-#endif /*_XMALLOC_H*/
